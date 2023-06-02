fd97375f6663055270e11b8fbadc0430e76c5a71
Marc Ströbel <m.stroebel@gmx.de>
2023-06-02 16:10:28 +0200

    initial

---
+++ src/app.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Res,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { exec } from 'child_process';
+import { Response } from 'express';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadFile(@UploadedFiles() files: Array<Express.Multer.File>,
+    @Res() response: Response,
+  ) {
+    console.log(files);
+
+    const img2pdf_opts = files.map((file) => file.path).join(' ');
+
+    const img2pdf = exec(`img2pdf ${img2pdf_opts} out.pdf`);
+
+    img2pdf.on('exit', () => {
+      const ocrmypdf = exec('ocrmypdf out.pdf out.pdf');
+
+      ocrmypdf.on('exit', () => {
+        const file = createReadStream(join(process.cwd(), 'out.pdf'));
+        file.pipe(response);
+      });
+    });
+  }
+}
