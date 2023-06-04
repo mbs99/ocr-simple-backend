b550609e5c982984a39e83701c080ef25721c626
Marc Ströbel <m.stroebel@gmx.de>
2023-06-04 16:52:30 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,6 @@ import { unlink } from 'fs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // automatically deletes uploaded files when express finishes the request
-  app.use(function (req, res, next) {
-    const writeHead = res.writeHead;
-    const writeHeadbound = writeHead.bind(res);
-    res.writeHead = function (statusCode, statusMessage, headers) {
-      if (req.files) {
-        for (const file of req.files) {
-          unlink(file.path, function (err) {
-            if (err) console.error(err);
-          });
-        }
-      }
-
-      writeHeadbound(statusCode, statusMessage, headers);
-    };
-
-    next();
-  });
-
   await app.listen(3000);
 }
 bootstrap();
