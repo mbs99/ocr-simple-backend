196cd11d98f3728ba95829fe2bc580f075999301
Marc Ströbel <m.stroebel@gmx.de>
2023-06-05 11:22:30 +0200

    render params

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { renderFile } from 'pug';
+import { LocalsObject, renderFile } from 'pug';
 
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
   getIndex(): string {
-    return renderFile(`${__dirname}/../templates/index.pug`);
+    const params: LocalsObject = {
+      title: 'ocr-simple-backend',
+    };
+
+    return renderFile(`${__dirname}/../templates/index.pug`, params);
   }
 }
