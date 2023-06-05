bb5f2c0274c428de7478b1efac9dbfb58ac8b58e
Marc Ströbel <m.stroebel@gmx.de>
2023-06-05 20:10:25 +0200

    refactored

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -33,59 +33,7 @@ export class AppController {
     @Res() response: Response,
     @Body() form,
   ) {
-
-    this.logger.log(form);
-
-    const tempfile = (await import('tempfile')).default;
-    const imagesPdfFile = tempfile({ extension: 'pdf' });
-    this.logger.log(imagesPdfFile);
-
-    const filePageMap = new Map<number, string>();
-    files.forEach((file) => {
-      const name = file.originalname;
-      filePageMap.set(form[name], name);
-    });
-
-    const sortedFilePageMap = new Map([...filePageMap.entries()].sort());
-
-
-    const img2pdf_opts = files.map((file) => file.path).join(' ');
-
-    const img2pdf = exec(
-      `img2pdf ${img2pdf_opts} -o ${imagesPdfFile}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          this.logger.log('STDOUT:', stdout, ', STDERR:', stderr);
-        }
-      },
-    );
-
-    img2pdf.on('exit', (code) => {
-      if (0 == code) {
-        const ocrmypdfCmdline = `ocrmypdf -l deu ${imagesPdfFile} ${imagesPdfFile}`;
-        console.log(ocrmypdfCmdline);
-        const ocrmypdf = exec(ocrmypdfCmdline, (error, stdout, stderr) => {
-          if (error) {
-            this.logger.log('STDOUT:', stdout, ', STDERR:', stderr);
-          }
-        });
-
-        ocrmypdf.on('exit', (code) => {
-          if (0 == code) {
-            const result = createReadStream(imagesPdfFile);
-            result.on('end', () => {
-              unlink(imagesPdfFile, () => {
-                /* deleted */
-              });
-            });
-            result.pipe(response);
-          } else {
-            response.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
-          }
-        });
-      } else {
-        response.status(HttpStatus.INTERNAL_SERVER_ERROR).send();
-      }
-    });
+    const resultStream = await this.appService.ocrmypdf(files, form);
+    resultStream.pipe(response);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { exec } from 'child_process';
+import { ReadStream, createReadStream, unlink } from 'fs';
 import { LocalsObject, renderFile } from 'pug';
 
 @Injectable()
@@ -10,5 +12,67 @@ export class AppService {
     };
 
     return renderFile(`${__dirname}/../templates/index.pug`, params);
+  }
+
+  async ocrmypdf(
+    files: Array<Express.Multer.File>,
+    form: any,
+  ): Promise<ReadStream> {
+    const tempfile = (await import('tempfile')).default;
+    return new Promise((resolve, reject) => {
+      const imagesPdfFile = tempfile({ extension: 'pdf' });
+      this.logger.log(imagesPdfFile);
+
+      const filePageMap = new Map<number, Express.Multer.File>();
+      files.forEach((file) => {
+        const name = file.originalname;
+        filePageMap.set(form[name], file);
+      });
+
+      const sortedFilePageMap = new Map([...filePageMap.entries()].sort());
+
+      const img2pdf_opts = Array.from(sortedFilePageMap.values())
+        .map((file) => file.path)
+        .join(' ');
+
+      const img2pdf = exec(
+        `img2pdf ${img2pdf_opts} -o ${imagesPdfFile}`,
+        (error, stdout, stderr) => {
+          if (error) {
+            this.logger.log('STDOUT:', stdout, ', STDERR:', stderr);
+            reject(error);
+          }
+        },
+      );
+
+      img2pdf.on('exit', (code) => {
+        if (0 == code) {
+          const ocrmypdfCmdline = `ocrmypdf -l deu ${imagesPdfFile} ${imagesPdfFile}`;
+          console.log(ocrmypdfCmdline);
+          const ocrmypdf = exec(ocrmypdfCmdline, (error, stdout, stderr) => {
+            if (error) {
+              this.logger.log('STDOUT:', stdout, ', STDERR:', stderr);
+              reject(error);
+            }
+          });
+
+          ocrmypdf.on('exit', (code) => {
+            if (0 == code) {
+              const result = createReadStream(imagesPdfFile);
+              result.on('end', () => {
+                unlink(imagesPdfFile, () => {
+                  /* deleted */
+                });
+              });
+              resolve(result);
+            } else {
+              reject();
+            }
+          });
+        } else {
+          reject();
+        }
+      });
+    });
   }
 }
